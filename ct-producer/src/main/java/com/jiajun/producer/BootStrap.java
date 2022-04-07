68a35d507739f6a49e6a5148289f8a751cf3e2e4
Jiajun <Jiajun@ligong.com>
2022-04-07 21:58:02 +0800

    first-commit

--- ct-producer/src/main/java/com/jiajun/producer/BootStrap.java
+++ ct-producer/src/main/java/com/jiajun/producer/BootStrap.java
@@ -10,15 +10,19 @@ import java.io.IOException;
 public class BootStrap {
     public static void main(String[] args) throws IOException {
 
+        if( args.length < 2 ){
+            System.out.println("参数不正确,正确格式为：java -jar XXX.jar path1 path2");
+            System.exit(1);
+        }
+
         //构建生产者对象
         Producer pro = new LocalFileProducer();
-
-        pro.setInput(new LocalFileDataIn("E:\\我的文件\\contact.log"));
-        pro.setOut(new LocalFileDataOut("E:\\我的文件\\call.log"));
-
+        pro.setInput(new LocalFileDataIn(args[0]));
+        pro.setOut(new LocalFileDataOut(args[1]));
+//        pro.setInput(new LocalFileDataIn("E:\\我的文件\\contact.log"));
+//        pro.setOut(new LocalFileDataOut("E:\\我的文件\\call.log"));
         //生产数据
         pro.produce();
-
         //关闭对象
         pro.close();
     }
